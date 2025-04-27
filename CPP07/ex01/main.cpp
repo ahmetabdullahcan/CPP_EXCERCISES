4ddad7e822ee867bd532d3c98c8264c9d65f1e61
ahmetabdullahcan <ahmetabdullahcan@outlook.com>
2025-04-27 17:07:46 +0300

    CPP07 Updated

--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -1,5 +1,17 @@
 #include "iter.hpp"
 
+int sum(int &x) {
+    return x + 1;
+}
+
+void sums(std::string &x) {
+    x += "!";
+}
+
+int square(int &x) {
+    return x * x;
+}
+
 int main(void){
     int arr[] = {1, 2, 3, 4, 5};
     size_t length = sizeof(arr) / sizeof(arr[0]);
@@ -7,7 +19,7 @@ int main(void){
     iter(arr, length, [](int &x) { std::cout << x << " "; });
     std::cout << std::endl;
 
-    iter(arr, length, [](int &x) { x *= 2; });
+    iter(arr, length, [](int &x) { sum(x); });
     iter(arr, length, [](int &x) { std::cout << x << " "; });
     std::cout << std::endl;
 
@@ -15,7 +27,7 @@ int main(void){
     size_t strLength = sizeof(strArr) / sizeof(strArr[0]);
     iter(strArr, strLength, [](std::string &x) { std::cout << x << " "; });
     std::cout << std::endl;
-    iter(strArr, strLength, [](std::string &x) { x += "!!"; });
+    iter(strArr, strLength, [](std::string &x) { sums(x); });
     iter(strArr, strLength, [](std::string &x) { std::cout << x << " "; });
     std::cout << std::endl;
 
